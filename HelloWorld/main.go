3990f34e10a244325fd90efcdc3dd70e36b14168
ArnulfoVargas <vargas.mejia.arnulfo04@gmail.com>
2023-12-03 10:38:41 -0600

    declaration

--- HelloWorld/main.go
+++ HelloWorld/main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
+	// Comment
+	/* Also a comment */
 	fmt.Println("Hello world")
+	
+	// Long variable declaration 
+	var hello string = `Hello
+Bonjour
+Hola`
+
+	// Short variable declaration
+	text := `Hello
+Bonjour
+Hola`
+	
+	fmt.Println(hello)
+	fmt.Println(text)
 }
