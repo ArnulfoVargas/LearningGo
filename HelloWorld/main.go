714ad215a3782fbda2eabb731692b8984a9f3065
ArnulfoVargas <vargas.mejia.arnulfo04@gmail.com>
2023-12-03 10:41:03 -0600

    comments

--- HelloWorld/main.go
+++ HelloWorld/main.go
@@ -16,6 +16,12 @@ Hola`
 	text := `Hello
 Bonjour
 Hola`
+
+	// The := operator can be used only for initial value declarations
+	// If you declare a variable without a initial value, the value will be the default value
+	// int/float = 0
+	// string = ""
+	// bool = false
 	
 	fmt.Println(hello)
 	fmt.Println(text)
