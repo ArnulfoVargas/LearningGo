031d88b23a848362b1e76716c5d4fd2730f0d9f4
ArnulfoVargas <vargas.mejia.arnulfo04@gmail.com>
2023-12-03 20:38:25 -0600

    go basis

--- HelloWorld/main.go
+++ HelloWorld/main.go
@@ -79,7 +79,7 @@ Hola`
 	} else {
 		fmt.Println("Odd", randomNum2)
 	}
-	// randomNum2 doesnt exists at this point
+	// randomNum2 no longer exists at this point
 
 	// Switch
 	var letter rune = 'a'
@@ -136,4 +136,32 @@ Hola`
 	}
 
 	// Loops
+	
+	// Kind of loop in rust or While true in other languajes
+	for {
+		fmt.Print("Close? (y/n): ")
+		var c rune;
+		
+		fmt.Scanf("%c\n", &c)
+
+		if c == 'n' || c == 'N'{
+			continue
+		}
+		if c == 'y' || c == 'Y'{
+			break
+		}
+		fmt.Println("Not recognized")
+	}
+
+	fmt.Println("First loop closed")
+
+	var c rune;
+	for c != 'y' && c != 'Y'{
+		fmt.Print("Close? (y/n): ")
+		fmt.Scanf("%c\n", &c)
+	}
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
 }
